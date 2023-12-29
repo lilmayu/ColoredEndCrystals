78f4995cf8f9bbf310d01abd1512174af8c7d855
lilmayu <uwulilmayu@gmail.com>
2023-12-29 19:03:28 +0100

    Backport: Logging, v1.0.1

--- fabric/src/main/java/dev/mayuna/coloredendcrystals/fabric/ColoredEndCrystalsFabric.java
+++ fabric/src/main/java/dev/mayuna/coloredendcrystals/fabric/ColoredEndCrystalsFabric.java
@@ -7,11 +7,18 @@ import dev.mayuna.coloredendcrystals.entities.ColoredEndCrystalEntity;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.UseEntityCallback;
 import net.minecraft.world.InteractionResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ColoredEndCrystalsFabric implements ModInitializer {
 
+    // Logger
+    public static final Logger LOGGER = LoggerFactory.getLogger(ColoredEndCrystals.MOD_ID);
+
     @Override
     public void onInitialize() {
+        LOGGER.info("Hello >_<");
+        LOGGER.info("Initializing Colored End Crystals...");
         ColoredEndCrystals.init();
 
         // Register the UseEntityCallback event
@@ -29,5 +36,7 @@ public class ColoredEndCrystalsFabric implements ModInitializer {
             coloredEndCrystal.onRightClick(player.isShiftKeyDown());
             return InteractionResult.SUCCESS;
         });
+
+        LOGGER.info("Initialized Colored End Crystals! Have fun :3");
     }
 }
