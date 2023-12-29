45fb3b763e76eab9c24c3deb8d319ea204aec14d
lilmayu <uwulilmayu@gmail.com>
2023-12-29 19:01:36 +0100

    Backport: Logging, v1.0.1

--- forge/src/main/java/dev/mayuna/coloredendcrystals/forge/ColoredEndCrystalsForge.java
+++ forge/src/main/java/dev/mayuna/coloredendcrystals/forge/ColoredEndCrystalsForge.java
@@ -11,15 +11,23 @@ import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(ColoredEndCrystals.MOD_ID)
 public class ColoredEndCrystalsForge {
 
+    // Logger
+    public static final Logger LOGGER = LogManager.getLogger(ColoredEndCrystals.MOD_ID);
+
     public ColoredEndCrystalsForge() {
+        LOGGER.info("Hello >_<");
+        LOGGER.info("Initializing Colored End Crystals...");
         EventBuses.registerModEventBus(ColoredEndCrystals.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         ColoredEndCrystals.init();
 
         MinecraftForge.EVENT_BUS.register(this);
+        LOGGER.info("Initialized Colored End Crystals! Have fun :3");
     }
 
     @SubscribeEvent
